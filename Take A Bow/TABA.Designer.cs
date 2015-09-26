9cc735a17fb6dd9a904c9c3f7c2400c28fbc6901
Blissgig <blissgig@gmail.com>
2015-09-26 13:01:26 -0400

    Add to repo

---
+++ Take A Bow/TABA.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Take_A_Bow
+{
+    partial class TABA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TABA));
+            this.lnkSource = new System.Windows.Forms.LinkLabel();
+            this.cmdStartStop = new System.Windows.Forms.Button();
+            this.TABTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lnkSource
+            // 
+            this.lnkSource.AutoSize = true;
+            this.lnkSource.Location = new System.Drawing.Point(12, 166);
+            this.lnkSource.Name = "lnkSource";
+            this.lnkSource.Size = new System.Drawing.Size(115, 13);
+            this.lnkSource.TabIndex = 0;
+            this.lnkSource.TabStop = true;
+            this.lnkSource.Text = "Adam Frank: Performer";
+            this.lnkSource.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkSource_LinkClicked);
+            // 
+            // cmdStartStop
+            // 
+            this.cmdStartStop.Location = new System.Drawing.Point(160, 166);
+            this.cmdStartStop.Name = "cmdStartStop";
+            this.cmdStartStop.Size = new System.Drawing.Size(112, 40);
+            this.cmdStartStop.TabIndex = 1;
+            this.cmdStartStop.Text = "Stop Sensor";
+            this.cmdStartStop.UseVisualStyleBackColor = true;
+            this.cmdStartStop.Click += new System.EventHandler(this.cmdStartStop_Click);
+            // 
+            // TABTrayIcon
+            // 
+            this.TABTrayIcon.BalloonTipText = "Take A Bow";
+            this.TABTrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TABTrayIcon.Icon")));
+            this.TABTrayIcon.Text = "Take A Bow";
+            this.TABTrayIcon.Visible = true;
+            this.TABTrayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TAQBTrayIcon_MouseDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(260, 139);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // TABA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 212);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmdStartStop);
+            this.Controls.Add(this.lnkSource);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "TABA";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Take A Bow";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TABA_FormClosing);
+            this.Load += new System.EventHandler(this.TABA_Load);
+            this.SizeChanged += new System.EventHandler(this.TABA_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel lnkSource;
+        private System.Windows.Forms.Button cmdStartStop;
+        private System.Windows.Forms.NotifyIcon TABTrayIcon;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Take A Bow/TABA.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using System.IO;
+
+namespace Take_A_Bow
+{
+    public partial class TABA : Form
+    {
+        #region Private Members
+            private KinectSensor TABSensor;
+            private bool IsAudioPlaying = false;
+        #endregion
+
+
+        public TABA()
+        {
+            InitializeComponent();
+        }
+
+        private void TABA_Load(object sender, EventArgs e)
+        {
+            SensorStart();
+        }
+
+        private void SensorStart()
+        {
+            try
+            {
+                foreach (var potentialSensor in KinectSensor.KinectSensors)
+                {
+                    if (potentialSensor.Status == KinectStatus.Connected)
+                    {
+                        this.TABSensor = potentialSensor;
+                        break;
+                    }
+                }
+
+                if (null != this.TABSensor)
+                {
+                    // Turn on the skeleton stream to receive skeleton frames
+                    this.TABSensor.SkeletonStream.Enable();
+
+                    // Add an event handler to be called whenever there is new color frame data
+                    this.TABSensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+
+                    // Start the sensor!
+                    try
+                    {
+                        this.TABSensor.Start();
+                    }
+                    catch (IOException)
+                    {
+                        this.TABSensor = null;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Sorry, could not find a Kinect Device", "Kinect Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+            }
+        }
+
+        private void SensorStop()
+        {
+            try
+            {
+                if (null != this.TABSensor)
+                {
+                    this.TABSensor.Stop();
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);                              
+            }
+        }
+
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            try
+            {
+                Skeleton[] skeletons = new Skeleton[0];
+
+                using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+                {
+                    if (skeletonFrame != null)
+                    {
+                        skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                        skeletonFrame.CopySkeletonDataTo(skeletons);
+                    }
+                }
+
+                if (skeletons.Length != 0)
+                {
+                    foreach (Skeleton skel in skeletons)
+                    {
+                        if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            this.CheckBow(skel);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+            }
+        }
+
+        private void CheckBow(Skeleton skeleton)
+        {
+            try
+            {
+                if (IsAudioPlaying == false) //No need to suck CPU cycles if the audio is playing
+                {
+                    //Only activate if the user is not too close to sensor
+                    if (skeleton.Joints[JointType.HipCenter].Position.Z > 1.6)
+                    {
+                        float delta = Math.Abs(skeleton.Joints[JointType.HipCenter].Position.Y - skeleton.Joints[JointType.ShoulderCenter].Position.Y);
+
+                        if (delta < 0.21999)
+                        {
+                            IsAudioPlaying = true;
+                            PlayApplause();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+            }
+        }
+
+        private void PlayApplause()
+        {
+            try
+            {
+                Random objRandom = new Random();
+                UnmanagedMemoryStream RandomSound = Properties.Resources.Crickets;
+                byte bRandom = Convert.ToByte(objRandom.Next(0, 4));
+
+                switch (bRandom)
+                {
+                    case 0:
+                        RandomSound = Properties.Resources.Crickets;
+                        break;
+
+                    case 1:
+                        RandomSound = Properties.Resources.Applauding_People_Sound;
+                        break;
+
+                    case 2:
+                        RandomSound = Properties.Resources.Crowd_Applause;
+                        break;
+
+                    case 3:
+                        RandomSound = Properties.Resources.Polite_Applause;
+                        break;
+
+                    case 4:
+                        RandomSound = Properties.Resources.Small_Room;
+                        break;
+                }
+
+
+                System.Media.SoundPlayer ApplausePlayer = new System.Media.SoundPlayer(RandomSound);  //Properties.Resources.Crickets);
+
+                ApplausePlayer.PlaySync(); //To insure that the varaible IsPlaying doesn't get reset until the sound completes, as opposed to .Play()
+                IsAudioPlaying = false;    //only attempt a new sound when this one has stopped
+                ApplausePlayer = null;
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void TABA_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TABTrayIcon.Dispose();
+            SensorStop();
+        }
+
+        private void cmdStartStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Note: yes it is baf form to use the caption as the switch variable.  I know.
+                if (cmdStartStop.Text.ToString().Equals("Stop Sensor"))
+                {
+                    SensorStop();
+                    cmdStartStop.Text = "Start Sensor";
+                }
+                else
+                {
+                    SensorStart();
+                    cmdStartStop.Text = "Stop Sensor";
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+            }
+        }
+
+        private void lnkSource_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                this.lnkSource.LinkVisited = true;
+
+                System.Diagnostics.Process.Start("http://www.adamfrank.com/performer/performer.htm");
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+            }
+        }
+
+        private void TAQBTrayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void TABA_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = false;
+            }
+            else
+            {
+                this.Visible = true;
+            }
+        }
+
+        private void LogException(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
